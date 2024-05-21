5def30a7d2328a2da66a6d81747f2ddf7c5ca0a4
Alex SaM <alsam3637@gmail.com>
2024-05-21 15:09:28 -0400

    Custom Infolist Entry

--- app/Livewire/WatchEpisode.php
+++ app/Livewire/WatchEpisode.php
@@ -2,12 +2,22 @@
 
 namespace App\Livewire;
 
+use App\Infolists\Components\VideoPlayerEntry;
 use App\Models\Course;
 use App\Models\Episode;
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Forms\Contracts\HasForms;
+use Filament\Infolists;
+use Filament\Infolists\Concerns\InteractsWithInfolists;
+use Filament\Infolists\Contracts\HasInfolists;
+use Filament\Infolists\Infolist;
+use Illuminate\Support\Facades\App;
 use Livewire\Component;
 
-class WatchEpisode extends Component
+class WatchEpisode extends Component implements HasInfolists, HasForms
 {
+    use InteractsWithInfolists, InteractsWithForms;
+
     public Course $course;
 
     public Episode $currentEpisode;
@@ -21,6 +31,27 @@ class WatchEpisode extends Component
         } else {
             $this->currentEpisode = $course->episodes->first();
         }
+    }
+
+    public function episodeInfolist(Infolist $infolist): Infolist
+    {
+        return $infolist
+            ->record($this->currentEpisode)
+            ->schema([
+                VideoPlayerEntry::make('vimeo_id')
+                    ->hiddenLabel(),
+                Infolists\Components\TextEntry::make('overview'),
+                Infolists\Components\RepeatableEntry::make('course.episodes')
+                    ->schema([
+                        Infolists\Components\TextEntry::make('title')
+                            ->hiddenLabel()
+                            ->icon('heroicon-o-play-circle'),
+                        Infolists\Components\TextEntry::make('formatted_length')
+                            ->hiddenLabel()
+                            ->icon('heroicon-o-clock'),
+                    ])
+                    ->columns(2),
+            ]);
     }
 
     public function render()
