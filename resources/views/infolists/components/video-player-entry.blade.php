6c60224d7be81d4b6e034e40b60f95ccf0da7588
Alex SaM <alsam3637@gmail.com>
2024-05-22 16:35:34 -0400

    Watch Episode Page

--- resources/views/infolists/components/video-player-entry.blade.php
+++ resources/views/infolists/components/video-player-entry.blade.php
@@ -1,5 +1,16 @@
 <x-dynamic-component :component="$getEntryWrapperView()" :entry="$entry">
-    <div>
+    {{-- <div>
         <iframe src="https://player.vimeo.com/video/{{ $getState() }}"></iframe>        
+    </div> --}}
+    <div class="relative pt-[56.25%]">
+        <iframe
+            src="https://player.vimeo.com/video/{{ $getState() }}"
+            frameborder="0" 
+            allow="autoplay; fullscreen; picture-in-picture"
+            class="absolute left-0 top-0 w-full h-full">
+        </iframe>
+        @script
+            <script src="https://player.vimeo.com/api/player.js"></script>
+        @endscript
     </div>
 </x-dynamic-component>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Livewire\ShowCourse;
+use App\Livewire\WatchEpisode;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/courses/{course}', ShowCourse::class)->name('courses.show');
+Route::get('/courses/{course}/episodes/{episode?}', WatchEpisode::class)->name('courses.episodes.show');
